ad5788704031029463bad89fac519b1617f48218
oneApple <710406979@qq.com>
2013-03-14 20:11:57 +0800

    dh,aes

--- EnAndDecryption/Aes.py
+++ EnAndDecryption/Aes.py
@@ -1,14 +1,20 @@
-import BIO, RSA, EVP, util
+import BIO, RSA, EVP, util ,os
 class rsa:
+    def __init__(self,directory = "."):
+        if not os.path.exists(directory):
+            os.mkdir(directory)
+        self.prikeyDirectory = directory + "/" + "pri_key.pem"
+        self.pubkeyDirectory = directory + "/" + "pub_key.pem"
     def genRsaKeypair(self,rsalen = 1024):
         rsa_key = RSA.gen_key(rsalen, 3, lambda *arg:None) 
-        rsa_key.save_key("pri_key.pem", None)
-        rsa_key.save_pub_key("pub_key.pem")
+        
+        rsa_key.save_key(self.prikeyDirectory, None)
+        rsa_key.save_pub_key(self.pubkeyDirectory)
     def encryptionByPubkey(self,message):
-        pub_key = RSA.load_pub_key("pub_key.pem")
+        pub_key = RSA.load_pub_key(self.pubkeyDirectory)
         return pub_key.public_encrypt(message, RSA.pkcs1_padding)
     def decryptionByPrikey(self,message):
-        string = open("pri_key.pem","rb").read();
+        string = open(self.prikeyDirectory,"rb").read();
         bio = BIO.MemoryBuffer(string);
         pri_key = RSA.load_key_bio(bio);
         return pri_key.private_decrypt(message,RSA.pkcs1_padding)
@@ -16,14 +22,14 @@ class rsa:
         m=EVP.MessageDigest("sha1") 
         m.update(message) 
         digest=m.final() 
-        key_str=file("pri_key.pem","rb").read() 
+        key_str=file(self.prikeyDirectory,"rb").read() 
         key=RSA.load_key_string(key_str, util.no_passphrase_callback) 
         return key.sign(digest, "sha1") 
     def verifyByPubkeyAndSha1(self,sign,message):
         m=EVP.MessageDigest("sha1") 
         m.update(message) 
         digest=m.final() 
-        cert_str=file("pub_key.pem", "rb").read() 
+        cert_str=file(self.pubkeyDirectory, "rb").read() 
         mb=BIO.MemoryBuffer(cert_str) 
         cert=RSA.load_pub_key_bio(mb)
         try:
@@ -33,7 +39,7 @@ class rsa:
             return False
 
 if __name__ == '__main__':
-    r = rsa()
+    r = rsa("../key")
     r.genRsaKeypair()
     print r.decryptionByPrikey(r.encryptionByPubkey("message"))
-    print r.verifyByPubkeyAndSha1(r.signByPrikeyAndSha1("message"), "mesage")
+    print r.verifyByPubkeyAndSha1(r.signByPrikeyAndSha1("message"), "message")
